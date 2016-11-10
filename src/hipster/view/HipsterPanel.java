1f59fee6ed82efab8ce4e3e34f18b6e57266ac16
Bmar21 <brandonmalor21@gmail.com>
2016-11-10 13:44:01 -0700

    made the panel and frame for the hipster

--- src/hipster/view/HipsterPanel.java
+++ src/hipster/view/HipsterPanel.java
@@ -1,5 +1,59 @@
 package hipster.view;
 
-public class HipsterPanel {
+import javax.swing.*;
+import hipster.controller.HipsterController;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+
+public class HipsterPanel extends JPanel
+{
+	private HipsterController baseController;
+	private JButton myButton;
+	private JComboBox dropDown;
+	private JLabel wordsLabel;
+	private SpringLayout baseLayout;
+	
+	public HipsterPanel(HipsterController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		myButton = new JButton("Click here!!");
+		wordsLabel = new JLabel("such words!");
+		baseLayout = new SpringLayout();
+		dropDown = new JComboBox(baseController.getWords());
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.setBackground(Color.ORANGE);
+		this.add(myButton);
+		this.add(wordsLabel);
+		this.add(dropDown);
+	}
+	
+	private void setupLayout()
+	{
+		baseLayout.putConstraint(SpringLayout.SOUTH, myButton, -119, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, myButton, -131, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, dropDown, 79, SpringLayout.SOUTH, wordsLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, dropDown, 74, SpringLayout.WEST, this);
+	}
+	
+	private void setupListeners()
+	{
+		dropDown.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent selection)
+			{
+				String SelectedText = baseController.getWords()[dropDown.getSelectedIndex]
+			}
+		});
+	}
 }
